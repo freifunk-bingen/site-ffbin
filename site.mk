4a239891c7844f251633e489fbc699cffc5776f7
sbschuermann <stefan@freifunk-bingen.de>
2015-08-10 22:58:47 +0200

    Release-Bezeichnung korrigiert

--- site.mk
+++ site.mk
@@ -47,7 +47,7 @@ DEFAULT_GLUON_RELEASE := 0.6
 #			gluon-ff%site_code%-23.42+5-%router_model%.bin
 
 # Allow overriding the release number from the command line
-GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)-beta-0.1
+GLUON_RELEASE ?= gluon-$(DEFAULT_GLUON_RELEASE)-%site_code%-beta-0.1
 
 # Default priority for updates.
 GLUON_PRIORITY ?= 3
